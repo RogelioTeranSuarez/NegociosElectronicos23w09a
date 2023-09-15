ba3761eb716242b10035297905d5b7d8f68a44e5
carlos88200 <carlitos.mauricio08@hotmail.com>
2023-09-15 15:56:20 -0500

    carlos

--- src/components/AltasBajasCambios.vue
+++ src/components/AltasBajasCambios.vue
@@ -22,7 +22,7 @@
                     <td>{{ producto.nombre }}</td>
                     <td>{{ producto.Desc }}</td>
                     <td class="">
-                        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#myModal">
+                        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#myModaledit"  @click="llenar(index)">
                             Editar
                         </button>
                         <button type="button" class="btn btn-danger ml-2" data-toggle="modal" data-target="#myModal"
@@ -70,6 +70,41 @@
             </div>
         </div>
     </div>
+
+    <!-- Esta es tu ventana modal para Editar -->
+    <div class="modal fade" id="myModaledit">
+        <div class="modal-dialog">
+
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Editar producto </h5>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <!-- id -->
+                    <div class="input-group input-group-sm mb-3">
+                        <input id = "idedit" type="Number" class="form-control" placeholder="ID del producto" v-model="newProducto.Id">
+                    </div>
+
+                    <!-- Nombre-->
+                    <div class="input-group input-group-sm mb-3">
+                        <input type="text" class="form-control" placeholder="Nombre del producto"
+                            v-model="newProducto.Nombre">
+                    </div>
+
+                    <!-- Descripcion -->
+                    <div class="input-group input-group-sm mb-3">
+                        <input type="text" class="form-control" placeholder="Descripcion del producto"
+                            v-model="newProducto.Descripcion">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal" @click="UupdateProducto()">Guardar
+                        datos</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
   
 <script>
@@ -78,7 +113,9 @@ export default {
         return {
             Productos: [],
             newProducto: { id: "", nombre: '', Descripcion: "" },
-            UpdatePorducto: {id:""}
+            UpdatePorducto: {id:""},
+            ide: -1,
+            ide2: 0
         };
     },
     methods: {
@@ -100,9 +137,32 @@ export default {
         DeleteProducto() {
 
         },
-        UupdateProducto() {
+        UupdateProducto( ) {
+            const prodact={
+                id: this.newProducto.Id,
+                nombre: this.newProducto.Nombre,
+                Desc: this.newProducto.Descripcion
+            }
+            
+            this.Productos[this.ide2]= prodact;
+            
+            
+            this.newProducto.Id = "";
+            this.newProducto.Nombre = "";
+            this.newProducto.Descripcion = "";
+            
+
+        },
+        llenar(ide){
+            this.ide2 = ide;
+            var prod = this.Productos[ide];
+            this.newProducto.Id = prod.id;
+            this.newProducto.Nombre = prod.nombre;
+            this.newProducto.Descripcion = prod.Desc;
 
         }
+        
+        
     }
 }
 </script>
